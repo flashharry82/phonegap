04cb738f6ca93e2f44a09cc658b894a9cf163b7b
Alan Stirling <alan@attopartners.com>
2015-11-25 16:59:00 +0000

    alerts

--- www/js/draggable.js
+++ www/js/draggable.js
@@ -1,47 +1,74 @@
-var object = document.getElementById('draggable'),
-initX, initY, firstX, firstY;
+var initX, initY, firstX, firstY, movedX, movedY;
+var alertOn = true;
 
-object.addEventListener('mousedown', function(e) {
-
+function touchIt(e) {
 	e.preventDefault();
-	initX = this.offsetLeft;
-	initY = this.offsetTop;
-	firstX = e.pageX;
-	firstY = e.pageY;
+	var touch = e.originalEvent.touches;
 
-	this.addEventListener('mousemove', dragIt, false);
-
-	window.addEventListener('mouseup', function() {
-		object.removeEventListener('mousemove', dragIt, false);
-	}, false);
-
-}, false);
-
-object.addEventListener('touchstart', function(e) {
-
-	e.preventDefault();
-	initX = this.offsetLeft;
-	initY = this.offsetTop;
-	var touch = e.touches;
-	firstX = touch[0].pageX;
-	firstY = touch[0].pageY;
-
-	this.addEventListener('touchmove', swipeIt, false);
-
-	window.addEventListener('touchend', function(e) {
-		e.preventDefault();
-		object.removeEventListener('touchmove', swipeIt, false);
-	}, false);
-
-}, false);
-
-function dragIt(e) {
-	this.style.left = initX+e.pageX-firstX + 'px';
-	this.style.top = initY+e.pageY-firstY + 'px';
+	initX = this.offsetLeft; // draggable start y
+	initY = this.offsetTop; // draggable start x
+	if(e.type == 'touchstart'){
+		firstX = touch[0].pageX; // touch start x
+		firstY = touch[0].pageY; // touch start y
+		$(this).on('touchmove', moveIt); // add moving detection to draggable
+		$(window).on('touchend', $(this), endIt); // end touch on draggable
+	}
+	else if(e.type == 'mousedown') {
+		firstX = e.pageX; // click start x
+    firstY = e.pageY; // click start y
+		$(this).on('mousemove', moveIt); // add moving detection to draggable
+		$(window).on('mouseup', $(this), endIt); // end touch on draggable
+	}
 }
 
-function swipeIt(e) {
-	var contact = e.touches;
-	this.style.left = initX+contact[0].pageX-firstX + 'px';
-	this.style.top = initY+contact[0].pageY-firstY + 'px';
+function moveIt(e) {
+	e.preventDefault();
+	if(e.type == 'touchmove'){
+		var contact = e.originalEvent.touches;
+		movedX = contact[0].pageX
+		movedY = contact[0].pageY
+	}
+	else if(e.type == 'mousemove'){
+		movedX = e.pageX
+		movedY = e.pageY
+	}
+	var moveToX = initX+movedX-firstX;
+	var moveToY = initY+movedY-firstY;
+	var textArray = ['OWWWW!!!!','TOO HOT!!','STOP THAT!','ARGHH!','GET AWAY!'];
+	var randomNumber = Math.floor(Math.random()*textArray.length);
+
+	if(alertOn && tooHot(moveToY,moveToX)){
+		alert(textArray[randomNumber]);
+		alertOn = false;
+	}
+	else if(!tooHot(moveToY,moveToX)){
+		alertOn = true;
+	}
+	$(e.target).css('left',initX+movedX-firstX + 'px').css('top',initY+movedY-firstY + 'px');
+}
+
+function endIt(e) {
+	e.preventDefault();
+	if(e.type == 'touchend'){
+		e.data.off('touchmove');
+	}
+	else if(e.type == 'mouseup'){
+		e.data.off('mousemove');
+	}
+}
+
+function tooHot(moveToY,moveToX){
+	/*if (moveToY > 40 && moveToY < 120 && moveToX > -20 && moveToX < 60){
+		return true;
+	}*/
+	if (moveToY > -50 && moveToY < 30 && moveToX > 50 && moveToX < 130){
+		return true;
+	}
+	if (moveToY > 280 && moveToY < 370 && moveToX > 120 && moveToX < 210){
+		return true;
+	}
+	if (moveToY > 325 && moveToY < 415 && moveToX > -5 && moveToX < 85){
+		return true;
+	}
+	return false;
 }
